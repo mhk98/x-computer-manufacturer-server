1f87f0ff278cef471bf6afd6ccc677ff00e063a3
mhk98 <mohsinkabirseo@gmail.com>
2022-06-06 01:24:44 +0600

    payment success

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -38,6 +39,22 @@ async function run(){
         const userCollection = client.db('xComputer').collection('users');
         const reviewCollection = client.db('xComputer').collection('reviews');
 
+
+      app.post('/create-payment-intent', verifyJWT, async(req, res)=>{
+        const tool= req.body;
+        const price = tool.price;
+        const amount= price*100;
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: "usd",
+          "payment_method_types": ["card"],
+        });
+        res.send({
+          clientSecret: paymentIntent.client_secret,
+        });
+      
+
+      })
 
         //GET
         app.get('/tool',  async(req, res)=>{
