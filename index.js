e64db6b09268960407df35405ba54f274e73bd63
Md Mohsin Kabir <mohsinkabirseo@gmail.com>
2022-06-29 16:42:46 +0600

    Nodemon added

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
+// app.use(cors({origin: 'https://x-computer-manufacture.web.app/'})
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y3bgh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
